ca144259de109ed6e8cef380ba9d8662b5b1cfb4
ElPatoDelCuak <davidjuncosaalegre@gmail.com>
2025-11-20 20:16:42 +0100

    Third commit

--- ProjecteSetmana/config/urls.py
+++ ProjecteSetmana/config/urls.py
@@ -20,5 +20,5 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
 
-    path('setmana', include('setmana.urls')),
+    path('setmana/', include('setmana.urls')),
 ]
