2ffee7c8283803a6b973e90b24c64653c92cd77e
ElPatoDelCuak <davidjuncosaalegre@gmail.com>
2025-11-27 16:30:09 +0100

    Casi acabado

--- ProjecteSetmana/setmana/urls.py
+++ ProjecteSetmana/setmana/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('<int:day>/', views.dia, name='dia'),
 ]
--- ProjecteSetmana/setmana/views.py
+++ ProjecteSetmana/setmana/views.py
@@ -1,7 +1,37 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-# Create your views here.
+from django.shortcuts import render, redirect
+from django.http import Http404
 
 def home(request):
     return render(request, "setmana/home.html")
+
+
+def dia(request, day: int):
+    if day <= 0:
+        return redirect('home')
+
+    dias = {
+        1: 'Dilluns',
+        2: 'Dimarts',
+        3: 'Dimecres',
+        4: 'Dijous',
+        5: 'Divendres',
+        6: 'Dissabte',
+        7: 'Diumenge',
+    }
+
+    img = {
+        1: '/static/dilluns.jpg',
+        2: '/static/dimarts.jpg',
+        3: '/static/dimecres.jpg',
+        4: '/static/dijous.jpg',
+        5: '/static/divendres.jpg',
+        6: '/static/dissabte.jpg',
+        7: '/static/diumenge.jpg',
+    }
+
+    try:
+        nombre = dias[day]
+    except KeyError:
+        raise Http404('Dia no valid')
+
+    return render(request, 'setmana/dies.html', {'day': day, 'day_name': nombre , 'image_url': img[day]})
