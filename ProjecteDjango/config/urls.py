65f53707282d82bf8b082f072d0b464659421a1b
ElPatoDelCuak <davidjuncosaalegre@gmail.com>
2025-12-04 19:02:11 +0100

    P2

--- ProjecteDjango/config/urls.py
+++ ProjecteDjango/config/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
 
     path('setmana/', include('setmana.urls')),
+    path('templates/', include('templates.url'))
 ]
---
+++ ProjecteDjango/setmana/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render, redirect
+from django.http import Http404
+
+def home(request):
+    return render(request, "setmana/home.html")
+
+
+def dia(request, day: int):
+    if day <= 0:
+        return redirect('home')
+
+    dias = {
+        1: 'dilluns',
+        2: 'dimarts',
+        3: 'dimecres',
+        4: 'dijous',
+        5: 'divendres',
+        6: 'dissabte',
+        7: 'diumenge',
+    }
+
+    try:
+        nombre = dias[day]
+    except KeyError:
+        raise Http404('Dia no valid, introdueix un nombre entre 1 i 7, o 0 y negatius per tornar a la pàgina principal.')
+
+    return render(request, 'setmana/dies.html', {'day': day, 'day_name': nombre , 'image_url': dias[day]})
