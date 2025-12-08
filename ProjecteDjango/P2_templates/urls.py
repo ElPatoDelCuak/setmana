f22f040efe5f965f305d5a9bdaf704e8c8d8011c
David <194682261+ElPatoDelCuak@users.noreply.github.com>
2025-12-08 17:55:26 +0100

    acabdo ya

--- ProjecteDjango/P2_templates/urls.py
+++ ProjecteDjango/P2_templates/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('home/', views.home, name='home'),
     path('<str:personatge>/', views.personatge, name='personatge'),
+    # Ruta per a personatge específic
 ]
--- ProjecteDjango/P2_templates/views.py
+++ ProjecteDjango/P2_templates/views.py
@@ -1,34 +1,34 @@
 from django.shortcuts import render
 
-
+#Diccionari de personatges
 personatges = {
     "Johnny_Silverhand": {
         "nom" : "Johnny Silverhand",
-        "frase" : "Never fade away",
+        "frase" : "I saw corps ... transform Night City into a machine fueled by people's crushed spirits, broken dreams and emptied pockets. Corps've long controlled our lives, taken lots... and now they're after our souls! ... I've declared war not 'cause capitalism's a thorn in my side or outta nostalgia for an America gone by. This war's a people's war against a system that's spiralled outta our control.",
         "img" : "/static/img/jhonny.png",
         "opcio" : "/P2_templates/Johnny_Silverhand/"
     },
     "John_Hammond": {
         "nom" : "John Hammond",
-        "frase" : "Welcome to Jurassic Park",
+        "frase" : "Welcome to Jurassic Park!",
         "img" : "/static/img/john.png",
         "opcio" : "/P2_templates/John_Hammond/"
     },
     "Obi_Wan_Kenobi": {
         "nom" : "Obi-Wan Kenobi",
-        "frase" : "May the Force be with you",
+        "frase" : "Hello there",
         "img" : "/static/img/obi-wan.png",
         "opcio" : "/P2_templates/Obi_Wan_Kenobi/"
     },
     "Arthur_Morgan": {
         "nom" : "Arthur Morgan",
-        "frase" : "We are thieves in a world that don't want us no more",
+        "frase" : "We’re Thieves In A World That Don’t Want Us No More",
         "img" : "/static/img/arthur.png",
         "opcio" : "/P2_templates/Arthur_Morgan/"
     },
     "Lara_Croft" : {
         "nom" : "Lara Croft",
-        "frase" : "Adventure is out there",
+        "frase" : "When Life Flashes Before Us, We Find Something; Something That Keeps Us Going.",
         "img" : "/static/img/lara.png",
         "opcio" : "/P2_templates/Lara_Croft/"
     }
@@ -36,10 +36,13 @@ personatges = {
 
 def home(request):
     return render(request, "P2_templates/home.html", {"personatges": personatges.values()})
+    #Vista de personatges
 
+#Vista de personatge específic
 def personatge(request, personatge):
     if personatge in personatges:
-        persontage = personatges[personatge]
-        return render(request, "P2_templates/personatge.html", persontage)
+        context = personatges[personatge]
+        return render(request, "P2_templates/home.html", context)
     else:
-        return render(request, "P2_templates/error.html", status=404)
+        return render(request, "P2_templates/home.html", {"error": True}, status=404)
+    #Vista d'error si el personatge no existeix al diccionari
